b4d6879c175bae86b4cbe5cefb1fd2126d6b72f9
arabella-jordan <faearabellajordan.philippian@gmail.com>
2023-08-09 12:05:50 +0800

    init

--- resources/views/livewire/user/create.blade.php
+++ resources/views/livewire/user/create.blade.php
@@ -1,13 +1,15 @@
-<div class="p-10 m-auto my-5 bg-white shadow-2xl lg:w-fit-content sm:w-full">
-    <h2 class="text-center">Create User</h2>
+<div class="p-5">
+    <div class="w-full mx-auto mt-[70px] bg-white shadow-2xl p-7 md:w-3/5">
+        <h2 class="text-center">Create User</h2>
 
-    <div>
-        <x-input class="mb-5" label="First Name" placeholder="First Name" wire:model="firstName" />
-        <x-input class="mb-5" label="Last Name" placeholder="Last Name" wire:model="lastName" />
-        <x-input class="mb-5" label="Middle Name" placeholder="Last Name" wire:model="middleName" />
-        <x-input class="mb-5" label="Email" placeholder="email@email.com" wire:model="email" />
-        <x-inputs.password class="mb-5" label="Password" placeholder="password" wire:model="password" />
-        <x-inputs.password class="mb-5" label="Confirm Password" placeholder="Confirm Password" wire:model="password_confirmation" />
-        <x-button class="w-full text-white bg-gradient-to-r from-red-500 to-pink-500" label="Save" wire:click='create' />
+        <div>
+            <x-input class="mb-5" label="First Name" placeholder="First Name" wire:model="firstName" />
+            <x-input class="mb-5" label="Last Name" placeholder="Last Name" wire:model="lastName" />
+            <x-input class="mb-5" label="Middle Name" placeholder="Last Name" wire:model="middleName" />
+            <x-input class="mb-5" label="Email" placeholder="email@email.com" wire:model="email" />
+            <x-inputs.password class="mb-5" label="Password" placeholder="password" wire:model="password" />
+            <x-inputs.password class="mb-5" label="Confirm Password" placeholder="Confirm Password" wire:model="password_confirmation" />
+            <x-button class="w-full text-white bg-gradient-to-r from-red-500 to-red-700" label="Save" wire:click='create' />
+        </div>
     </div>
 </div>
--- resources/views/livewire/user/index.blade.php
+++ resources/views/livewire/user/index.blade.php
@@ -1,6 +1,5 @@
-<div>
+<div class="pt-[60px]">
     {{-- Will show the table in user table. --}}
-    <h2 class="p-6 m-0 text-xl font-semibold leading-tight text-gray-800 bg-white shadow max-w-7xl sm:px-6 lg:px-8 px-8/9">User Index</h2>
     <div class="p-5">
         <livewire:tables.user-table>
     </div>
